7c252140b576d3cf0958f1da3bec191b3f2c8f5a
José de Bortoli <josedebortoli08@gmail.com>
2026-04-20 13:37:09 -0300

    alt2

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello, World!')
+import streamlit as st
+
+st.tile("Hello, Streamlit!")
+st.header("Welcome to Streamlit")
+st.subheader("This is a simple Streamlit app.")
+
+name = st.text_input("Enter your name:")
+if name:
+    st.write(f"Hello, {name}! Welcome to Streamlit.")
+
+number = st.number_input("Enter a number:", min_value=0, max_value=100)
+st .write(f"You entered the number: {number}")
